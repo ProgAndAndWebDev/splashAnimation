8e50f9c338240709e2a90fe25747b3853e719de5
shahad tariq <shahadtariq9@gmail.com>
2021-08-28 01:54:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/splashanimation/OnBoardingFragment3.java
@@ -0,0 +1,21 @@
+package com.example.splashanimation;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class OnBoardingFragment3 extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(
+                R.layout.fragment_on_boarding3,
+                container,false);
+        return root;
+    }
+}
---
+++ app/src/main/java/com/example/splashanimation/ScreenSlidePagerAdapter.java
@@ -0,0 +1,22 @@
+package com.example.splashanimation;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+
+    public ScreenSlidePagerAdapter( FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
